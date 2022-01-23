68170ab5f4431b744e9b400cba7e3fcacef14c44
besartsherifi <59452085+besartsherifi@users.noreply.github.com>
2022-01-23 20:55:31 +0100

    Unit Testing

--- src/test/java/com/jobFinder/api/JobFinderApiApplicationTests.java
+++ src/test/java/com/jobFinder/api/JobFinderApiApplicationTests.java
@@ -1,15 +1,16 @@
 package com.jobFinder.api;
 
 import com.jobFinder.api.Pojo.Entity.Address;
-import com.jobFinder.api.Pojo.Entity.Bid;
+import com.jobFinder.api.Pojo.Entity.User;
 import com.jobFinder.api.Pojo.Entity.Work;
+import com.jobFinder.api.Repository.BidRepository;
 import com.jobFinder.api.Repository.UserRepository;
 import com.jobFinder.api.Repository.WorkRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class JobFinderApiApplicationTests {
@@ -20,23 +21,40 @@ class JobFinderApiApplicationTests {
 	@Autowired
 	UserRepository userRepository;
 
+	@Autowired
+	BidRepository bidRepository;
 
+	private static Address address = new Address("Tetovo","NMK");
 
 	@Test
-	public void testCreate(){
-		Work j = new Work();
-		Address address = new Address("Tetovo","NMK");
-		j.setId(1);
-		j.setTitle("Do Dishes");
-		j.setDescription("Doing Dishes");
-		j.setAddress(address);
-		workRepository.save(j);
+	public void testWork(){
+		Work work = new Work();
+		work.setId(1);
+		work.setTitle("Do Dishes");
+		work.setDescription("Doing Dishes");
+		work.setAddress(address);
+		workRepository.save(work);
 		assertNotNull(workRepository.findById(1));
 	}
 
 	@Test
-	public void getUsers(){
+	public void testGetUsers() {
 		assertNotNull(userRepository.findAll());
+	}
+
+	@Test
+	public void testUser() {
+		User user = new User();
+		user.setId(1);
+		user.setUsername("test");
+		user.setAddress(address);
+		User userInDB = userRepository.findUserById(1);
+		assertNotEquals(user,userInDB);
+	}
+
+	@Test
+	public void testBid() {
+		assertNull(bidRepository.findById(1));
 	}
 
 }
